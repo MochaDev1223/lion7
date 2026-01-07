213c8b401736a6c13f5fb0aaf9ee9a64659a35fb
mochadev1223 <mochadev1223@gmail.com>
2026-01-07 09:01:34 +0900

    260106_2 commit

---
+++ 260106_Q2/260106_Q2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _260106_Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //### 도전 문제 1: 크리티컬 데미지 계산
+            // 기본 공격력 100, 크리티컬 확률 25 % 일 때:
+            //-일반 공격 데미지
+            //-크리티컬 공격 데미지(1.5배)
+            //-4번 공격 시 평균 데미지
+
+            int baseAtk = 100;
+            float critRate = 0.25f;
+            var critDamage = baseAtk * 1.5f;
+            var averageDamage = (baseAtk * (1 - critRate) + (critDamage * critRate));
+
+
+
+            Console.WriteLine($"일반 공격 데미지 : {baseAtk}");
+            Console.WriteLine($"크리티컬 공격 데미지 : {critDamage}");
+            Console.WriteLine($"1회 공격 시 평균 데미지 : {averageDamage}");
+            Console.WriteLine($"4회 공격 시 평균 데미지 : {averageDamage * 4}");
+
+
+        }
+    }
+}
---
+++ 260106_Q3/260106_Q3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _260106_Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //### 도전 문제 2: 시간 변환기
+            // 게임 플레이 시간이 3725초일 때:
+            //-몇 시간인가요 ?
+            //-몇 분인가요 ?
+            //-몇 초인가요 ?
+            //(예: 1시간 2분 5초)
+
+            int playTime = 3725;
+            int hour = playTime / 3600;
+            int remainSec = playTime % 3600;
+
+            int min = remainSec / 60;
+            int sec = remainSec % 60;
+
+            Console.WriteLine($"{hour}시간 {min}분 {sec}초");
+
+        }
+    }
+}
